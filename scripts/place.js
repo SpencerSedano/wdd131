2c509490d76362ef1e88ac5e2d85617949e03511
SpencerSedano <spencer.sv20@gmail.com>
2024-05-07 22:09:47 +0800

    placeholder wind chill

--- scripts/place.js
+++ scripts/place.js
@@ -12,15 +12,26 @@ gettingYear.innerHTML = "© " + yearVar;
 let lastModification = new Date(document.lastModified);
 lastModified.innerHTML = lastModification;
 
-//Cusco Letter
-
-const cuscoLink = document.getElementById("cusco-link");
-const letters = document.querySelectorAll(".letter");
-
-cuscoLink.addEventListener("click", function (event) {
-  letters.forEach((letter, index) => {
-    setTimeout(() => {
-      letter.classList.add("clicked");
-    }, index * 100);
-  });
-});
+// WindChill
+function calculateWindChill(temperature, windSpeed, isMetric) {
+  if (
+    (isMetric && temperature <= 10 && windSpeed > 4.8) ||
+    (!isMetric && temperature <= 50 && windSpeed > 3)
+  ) {
+    return isMetric
+      ? (
+          13.12 +
+          0.6215 * temperature -
+          11.37 * Math.pow(windSpeed, 0.16) +
+          0.3965 * temperature * Math.pow(windSpeed, 0.16)
+        ).toFixed(2) + " °C"
+      : (
+          35.74 +
+          0.6215 * temperature -
+          35.75 * Math.pow(windSpeed, 0.16) +
+          0.4275 * temperature * Math.pow(windSpeed, 0.16)
+        ).toFixed(2) + " °F";
+  } else {
+    return "N/A";
+  }
+}
